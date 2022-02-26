a0e86fd6867cdb9bf81e8f84251aa9d6329fcecf
BuildTools <unconfigured@null.spigotmc.org>
2022-02-26 16:09:46 +0100

    Alle sprites werden nun gerendert nicht nur der Spieler

--- Projekt/projekt2.py
+++ Projekt/projekt2.py
@@ -4,9 +4,9 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode([1920/2, 1080/2], pygame.RESIZABLE)
-assets = {  "window_icon": pygame.image.load("Assets/window_icon.png").convert_alpha(),
-            "player_image": pygame.image.load("Assets/player_image.png").convert_alpha()}
-gamestate = {"player": None, "camera": None}
+assets = {  "window_icon": pygame.image.load("Projekt/Assets/window_icon.png").convert_alpha(),
+            "player_image": pygame.image.load("Projekt/Assets/player_image.png").convert_alpha()}
+gamestate = {"player": None, "camera": None, "all_entities": pygame.sprite.Group()}
 
 pygame.display.set_caption('Asteriodes by Rofdo')
 pygame.display.set_icon(assets["window_icon"])
@@ -14,16 +14,19 @@ pygame.display.set_icon(assets["window_icon"])
 class Camera():
     def __init__(self, position = pygame.Vector2(0, 0)) -> None:
         self.position = position
+
 gamestate["camera"] = Camera()
 
 class Entity(pygame.sprite.Sprite):
     def __init__(self, image: pygame.surface, size = pygame.Vector2(50, 50), position = pygame.Vector2(0, 0), rotation = 0) -> None:
+        super().__init__()
+        gamestate["all_entities"].add(self)
         self.rotation = rotation
         self.position = position
         self.size = size
         self.surf = image
         self.surf.set_colorkey((0, 0, 0), pygame.RLEACCEL)
-    
+
     def get_image(self) -> pygame.surface:
         image = pygame.transform.scale(self.surf, (self.size.x, self.size.y))
         image = pygame.transform.rotate(image, self.rotation)
@@ -50,12 +53,12 @@ gamestate["player"] = Player()
 
 def render():
     c: Camera = gamestate["camera"]
-    p: Player = gamestate["player"]
-    s: pygame.surface = p.get_image()
-    r: pygame.rect = s.get_rect()
-    screen.fill((0, 0, 0))
-    xs, ys = screen.get_size()
-    screen.blit(s, r.move((xs-s.get_width())/2, (ys-s.get_height())/2).move(p.position.x, p.position.y).move(-c.position.x, -c.position.y))
+    for e in gamestate["all_entities"]:
+        s: pygame.surface = e.get_image()
+        r: pygame.rect = s.get_rect()
+        screen.fill((0, 0, 0))
+        xs, ys = screen.get_size()
+        screen.blit(s, r.move((xs-s.get_width())/2, (ys-s.get_height())/2).move(e.position.x, e.position.y).move(-c.position.x, -c.position.y))
 
     pygame.display.flip()
 
@@ -67,6 +70,7 @@ def game_update() -> bool:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 run = False
+
     gamestate["player"].update(pygame.key.get_pressed())
 
     render()
